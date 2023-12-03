3c289be554b359478b92acf6fa67c5d5e59329a3
João Lucas Abreu <joaolucasabreunogueira@gmail.com>
2023-12-03 02:41:06 -0300

    complete

--- Program.cs
+++ Program.cs
@@ -1,14 +1,30 @@
 ﻿using SkiaSharp;
 
 namespace Projeto {
+
+	class Bola {
+		public int? x { get; set; }
+    	public int? y { get; set; }
+	}
+
 	class Program {
 		static void Main(string[] args) {
 			
 			Console.WriteLine("Número da cena: ");
-			int cena = Console.ReadLine();
+			string input = Console.ReadLine();
+			int.TryParse(input, out int cena);
 
-			Console.WriteLine("Quantidade de quadros na cena: ");
-			int quadros = Console.ReadLine();
+			Console.WriteLine("Número de quadros: ");
+			input = Console.ReadLine();
+			int.TryParse(input, out int quadros);
+
+			Bola ultimoLocalBola = new Bola {
+				x = null,
+				y = null,
+			};
+
+			List<double> velocidades = new List<double>();
+			int? primeiroQuadroMov = null;
 
 			for (int q = 0; q < quadros; q++)
 			{
@@ -21,167 +37,75 @@ namespace Projeto {
 						byte* saida = (byte*)bitmapSaida.GetPixels();
 
 						long pixelsTotais = bitmapEntrada.Width * bitmapEntrada.Height;
-						long media = 0;
-
-						// RGB para escala de cinza
-						for (int e = 0, s = 0; s < pixelsTotais; e += 4, s++) {
-							saida[s] = (byte)((entrada[e] + entrada[e + 1] + entrada[e + 2]) / 3);
-							media += saida[s];
-						}
-
-						media = (byte)(media / pixelsTotais);
 
 						// Limiarização
-						for(int p = 0; p < pixelsTotais; p++)
-						{
-							if(saida[p] > media)
-								saida[p] = 0;
-							else
-								saida[p] = 255;
+						for (int e = 0, s = 0; s < pixelsTotais; e += 4, s++) {
+							
+							if (entrada[e + 1] > entrada[e] && entrada[e + 1] > entrada[e + 2])
+								saida[s] = 0;
+							else 
+								saida[s] = 255;
 						}
 
 						// Erosão
 						Erodir(saida, bitmapSaida.Width, bitmapSaida.Height, 7, entrada);
+
+						var formas = Forma.DetectarFormas(saida, bitmapSaida.Width, bitmapSaida.Height, true);
+
+						bool temBola = false;
+						foreach (var forma in formas) 
+						{
+							//Console.WriteLine($"Quadro_{1}: Altura={forma.Altura} Largura={forma.Largura} Area={forma.Largura * forma.Altura}");
+							if (forma.Largura * forma.Altura < 8500) 
+							{
+								temBola = true;
+								if (ultimoLocalBola.x != null && ultimoLocalBola.x != null)
+								{
+									if ((ultimoLocalBola.x != forma.CentroX || ultimoLocalBola.y != forma.CentroY) && primeiroQuadroMov == null)
+										primeiroQuadroMov = q;
+
+									if (q > 0)
+									{
+										var cm = forma.Largura / 30;
+										var deltax = (double)ultimoLocalBola.x - forma.CentroX;
+										var deltay = (double)ultimoLocalBola.y - forma.CentroY;
+										var distancia = Math.Sqrt(deltax * deltax + deltay * deltay) * cm;
+										var velocidade = distancia/0.02;
+
+										Console.WriteLine($"{q - 1} ==> {q}: {velocidade:0.00}cm/s");
+
+										if (velocidade != 0)
+											velocidades.Add(velocidade);
+									}
+								}
+
+								ultimoLocalBola.x = forma.CentroX;
+								ultimoLocalBola.y = forma.CentroY;
+							}	
+						}
+
+						if (!temBola)
+							throw new Exception("Não tem bola");
 					}
 
-					using (FileStream stream = new FileStream("img\\erosed\\Gabarito Correto Erosed 1.png", FileMode.OpenOrCreate, FileAccess.Write)) {
+					using (FileStream stream = new FileStream($"img\\limiar\\Cena{cena}_{q}.png", FileMode.OpenOrCreate, FileAccess.Write)) {
 						bitmapSaida.Encode(stream, SKEncodedImageFormat.Png, 100);
 					}
 				}
 			}
 
+			if (primeiroQuadroMov != null) 
+				Console.WriteLine($"Primeiro quadro onde a bola se moveu: {primeiroQuadroMov}");
+			else 
+				Console.WriteLine("A bola não se moveu");
 
-
-
-
-
-
-
-
-
-			using (
-				SKBitmap bitmapEntrada = SKBitmap.Decode("img\\empty\\Gabarito Correto Vazio Cinza.png"),
-				bitmapSaida = new SKBitmap(new SKImageInfo(bitmapEntrada.Width, bitmapEntrada.Height, SKColorType.Gray8))) {
-				
-				unsafe {
-					byte* entrada = (byte*)bitmapEntrada.GetPixels();
-					byte* saida = (byte*)bitmapSaida.GetPixels();
-
-					long pixelsTotais = bitmapEntrada.Width * bitmapEntrada.Height;
-
-
-					for (int i = 0; i < pixelsTotais; i++)
-					{
-						saida[i] = entrada[i];
-					}
-
-					// Limiarização
-					for(int p = 0; p < pixelsTotais; p++)
-					{
-						if(saida[p] > 122)
-							saida[p] = 0;
-						else
-							saida[p] = 255;
-					}
-
-					var formas = Forma.DetectarFormas(saida, bitmapSaida.Width, bitmapSaida.Height, true);
-					int count = 1;
-					int row = 1;
-					foreach (var forma in formas){
-						if ((forma.Altura == 47 || forma.Altura == 48) && (forma.Largura == 47 || forma.Largura == 48))
-						{
-							string answer = "";
-							switch(count){
-								case 1:
-									answer = "A";
-									count++;
-									break;
-								case 2:
-									answer = "B";
-									count++;
-									break;
-								case 3:
-									answer = "C";
-									count++;
-									break;
-								case 4:
-									answer = "D";
-									count++;
-									break;
-								case 5:
-									answer = "E";
-									count = 1;
-									break;
-							}
-
-							slotsAlternativas.Add((x0: forma.X0, y0: forma.Y0, x1: forma.X1, y1: forma.Y1, row, answer));
-							if (count == 1){
-								row++;
-							}
-						}
-					}
-				}
-
-				using (FileStream stream = new FileStream("img\\erosed\\Gabarito Correto Vazio Cinza Limi.png", FileMode.OpenOrCreate, FileAccess.Write)) {
-					bitmapSaida.Encode(stream, SKEncodedImageFormat.Png, 100);
-				}
+			double vm = 0;
+			foreach (var v in velocidades)
+			{
+				vm = vm + v;
 			}
 
-			using (
-				SKBitmap bitmapEntrada = SKBitmap.Decode("img\\erosed\\Gabarito Correto Erosed 1.png"),
-				bitmapSaida = new SKBitmap(new SKImageInfo(bitmapEntrada.Width, bitmapEntrada.Height, SKColorType.Gray8))) {
-
-				unsafe {
-					byte* entrada = (byte*)bitmapEntrada.GetPixels();
-					var formas = Forma.DetectarFormas(entrada, bitmapEntrada.Width, bitmapEntrada.Height, true);
-
-					List<(int x0, int y0 , int x1 ,int y1)> quadrados = new List<(int x0, int y0 , int x1 ,int y1)> 
-					{
-						(32, 32, 82, 82),
-						(639, 32, 689, 82),
-						(32, 496, 82, 545),
-						(639, 496, 689, 545),
-						(32, 956, 82, 1009),
-						(639, 956, 689, 1009)
-					};
-
-					int count = 0;
-					for (int i = 0; i < formas.Count(); i++){
-						foreach(var quadrado in quadrados){
-							if(formas[i].FazInterseccao(quadrado.x0, quadrado.y0, quadrado.x1, quadrado.y1))
-								count++;
-						}
-					}
-
-					// Validação dos cantos 
-					if (count == 6) {
-						Console.WriteLine("RESULTADO DO GABARITO:" + "\n------------------------");
-						int row = 0;
-						int c  = 0;
-						foreach(var alternativa in slotsAlternativas){
-							if(row != alternativa.number){
-								if(row != 0 && c == 0){
-									Console.Write("Nenhuma Alternativa");
-								}
-								c = 0;
-								row = alternativa.number;
-								Console.Write("\n" + row + ": ");
-							}
-
-							foreach(var forma in formas){
-								if(forma.FazInterseccao(alternativa.x0, alternativa.y0, alternativa.x1, alternativa.y1)){
-									Console.Write(alternativa.answer + " ");
-									c++;
-									break;
-								}
-							}
-						}
-
-					} else {
-						Console.WriteLine("Gabarito Errado");
-					}
-				}
-			}
+			Console.WriteLine($"Velocidade da bola: {vm / velocidades.Count():0.00}cm/s");
 		}
 
 		static unsafe void Erodir(byte* imagem, int largura, int altura, int tamanhoJanela, byte* temp) {
